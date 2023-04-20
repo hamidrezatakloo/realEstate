110e0d7d9a79676dc83cd70b7f189ad74ea5c940
hamidreza <takloohamidreza@gmail.com>
2023-04-20 05:43:32 -0400

    Add a form to register users is singUp page
    The component is currently unstyled and does not implement any actual
    signUp logic. This is intended as a starting point for further development.

--- src/container/Signup.js
+++ src/container/Signup.js
@@ -1,5 +1,43 @@
+import { useState } from "react";
+import styles from "./Signup.module.css";
 const Signup = () => {
-  return <div>signup</div>;
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <div className={styles.signupPage}>
+      <div className={styles.container}>
+        <h1>حساب کاربری بسازید</h1>
+        <form className={styles.signupForm}>
+          <label>
+            نام کاربری
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </label>
+          <label>
+            ایمیل
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </label>
+          <label>
+            گذر واژه
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+          <button type="submit">ثبت نام</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
