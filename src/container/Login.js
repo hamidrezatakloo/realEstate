ad0f01cb4d13a97341b1b4fd8885405f69f1a42d
hamidreza <takloohamidreza@gmail.com>
2023-04-20 11:28:30 -0400

    Add form and store inputs with state
    This form has 2 input(email and password) to Login
    
    This component use auth css module for styling

--- src/container/Login.js
+++ src/container/Login.js
@@ -1,5 +1,36 @@
-const Login = () => {
-  return <div>Login</div>;
-};
+import { useState } from "react";
+import styles from "./auth.module.css";
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div className={styles.authPage}>
+      <div className={styles.container}>
+        <h1>ورود</h1>
+        <p>لطفا برای ورودایمیل و گذر واژه خود را وارد کنید</p>
+        <form className={styles.authForm}>
+          <label>
+            ایمیل
+            <input
+              type="email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
+          </label>
+          <label>
+            گذر واژه
+            <input
+              type="password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
+          </label>
+          <button type="submit">ورود</button>
+        </form>
+      </div>
+    </div>
+  );
+}
 
 export default Login;
