93b19630815cf001a70d87936e8df9343503669b
hamidreza <takloohamidreza@gmail.com>
2023-04-20 12:19:36 -0400

    Send credentials to server for verification

--- src/container/Login.js
+++ src/container/Login.js
@@ -4,12 +4,32 @@ function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    // send username and password to server
+    fetch("http://localhost:4000/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.log(error));
+
+    // reset form
+    setEmail("");
+    setPassword("");
+  }
+
   return (
     <div className={styles.authPage}>
       <div className={styles.container}>
         <h1>ورود</h1>
         <p>لطفا برای ورودایمیل و گذر واژه خود را وارد کنید</p>
-        <form className={styles.authForm}>
+        <form className={styles.authForm} onSubmit={handleSubmit}>
           <label>
             ایمیل
             <input
