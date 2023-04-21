f95dcaee7a3003493043653dc16ebcbde949c986
hamidreza <takloohamidreza@gmail.com>
2023-04-21 16:01:55 -0400

    Get location State with useContext hook

--- src/components/LocationMarker.js
+++ src/components/LocationMarker.js
@@ -1,15 +1,15 @@
-import { useState } from "react";
+import { useContext } from "react";
 import { Marker, Popup, useMapEvents } from "react-leaflet";
 import L from "leaflet";
 import icon from "leaflet/dist/images/marker-icon.png";
 import iconShadow from "leaflet/dist/images/marker-shadow.png";
-
+import LocationContext from "../contexts/LocationContext";
 let DefaultIcon = L.icon({
   iconUrl: icon,
   shadowUrl: iconShadow,
 });
 function LocationMarker() {
-  const [location, setLocation] = useState(null);
+  const { location, setLocation } = useContext(LocationContext);
   const map = useMapEvents({
     click: (event) => {
       const { lat, lng } = event.latlng;
