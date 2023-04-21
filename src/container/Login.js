01797f525c1b1b2b62909f8fd5aba391f6bd76b4
hamidreza <takloohamidreza@gmail.com>
2023-04-21 07:18:37 -0400

    Remove container from signUp and Login button

--- src/container/Login.js
+++ src/container/Login.js
@@ -25,30 +25,28 @@ function Login() {
   }
 
   return (
-    <div className={styles.authPage}>
-      <div className={styles.container}>
-        <h1>ورود</h1>
-        <p>لطفا برای ورودایمیل و گذر واژه خود را وارد کنید</p>
-        <form className={styles.authForm} onSubmit={handleSubmit}>
-          <label>
-            ایمیل
-            <input
-              type="email"
-              value={email}
-              onChange={(event) => setEmail(event.target.value)}
-            />
-          </label>
-          <label>
-            گذر واژه
-            <input
-              type="password"
-              value={password}
-              onChange={(event) => setPassword(event.target.value)}
-            />
-          </label>
-          <button type="submit">ورود</button>
-        </form>
-      </div>
+    <div className={styles.container}>
+      <h1>ورود</h1>
+      <p>لطفا برای ورودایمیل و گذر واژه خود را وارد کنید</p>
+      <form className={styles.authForm} onSubmit={handleSubmit}>
+        <label>
+          ایمیل
+          <input
+            type="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </label>
+        <label>
+          گذر واژه
+          <input
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </label>
+        <button type="submit">ورود</button>
+      </form>
     </div>
   );
 }
--- src/container/Signup.js
+++ src/container/Signup.js
@@ -20,38 +20,36 @@ const Signup = () => {
   }
 
   return (
-    <div className={styles.authPage}>
-      <div className={styles.container}>
-        <h1>ثبت نام</h1>
-        <p>لطفا برای ثبت نام اطلاعات خود را وارد کنید</p>
-        <form className={styles.authForm} onSubmit={handleSignup}>
-          <label>
-            نام کاربری
-            <input
-              type="text"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </label>
-          <label>
-            ایمیل
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </label>
-          <label>
-            گذر واژه
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </label>
-          <button type="submit">ثبت نام</button>
-        </form>
-      </div>
+    <div className={styles.container}>
+      <h1>ثبت نام</h1>
+      <p>لطفا برای ثبت نام اطلاعات خود را وارد کنید</p>
+      <form className={styles.authForm} onSubmit={handleSignup}>
+        <label>
+          نام کاربری
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <label>
+          ایمیل
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          گذر واژه
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button type="submit">ثبت نام</button>
+      </form>
     </div>
   );
 };
