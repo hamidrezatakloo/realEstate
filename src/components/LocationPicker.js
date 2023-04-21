40cdd52fc9dde5991110bb7086104bf00816184b
hamidreza <takloohamidreza@gmail.com>
2023-04-21 10:55:48 -0400

    Set flexGrow for locationPicker container
    Also Add some style such as boxShadow and some style to p tag like
    padding ,color and bg-color

--- src/components/LocationPicker.js
+++ src/components/LocationPicker.js
@@ -1,9 +1,10 @@
 import { MapContainer, TileLayer } from "react-leaflet";
 import LocationMarker from "./LocationMarker";
+import styles from "./locationPicker.module.css";
 function LocationPicker() {
   return (
-    <div>
-      <p>لوکیشن خود را از روی نقشه انتخاب کنید</p>
+    <div className={styles.container}>
+      <p className={styles.guide}>لوکیشن خود را از روی نقشه انتخاب کنید</p>
       <MapContainer center={[35.6892, 51.389]} zoom={13}>
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
         <LocationMarker />
