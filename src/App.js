8ade5d68848990bb07edece5f8e167aab1072f5d
hamidreza <takloohamidreza@gmail.com>
2023-04-21 07:18:01 -0400

    Use container from App css module in App component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import Login from "./container/Login";
 import Signup from "./container/Signup";
 import AddPost from "./container/AddPost";
+import styles from "./App.module.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/signup" element={<Signup />}></Route>
-        <Route path="/addpost" element={<AddPost />}></Route>
-      </Routes>
+      <div className={styles.container}>
+        <Routes>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
+          <Route path="/addpost" element={<AddPost />}></Route>
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
