9275d55e7cf8b7e109942e577248354687a55618
hamidreza <takloohamidreza@gmail.com>
2023-04-21 10:10:58 -0400

    Wrap MapContainer in a outer container
    Add a text with p tag for help of user

--- src/components/LocationPicker.js
+++ src/components/LocationPicker.js
@@ -1,9 +1,12 @@
 import { MapContainer, TileLayer } from "react-leaflet";
 function LocationPicker() {
   return (
-    <MapContainer center={[35.6892, 51.389]} zoom={13}>
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-    </MapContainer>
+    <div>
+      <p>لوکیشن خود را از روی نقشه انتخاب کنید</p>
+      <MapContainer center={[35.6892, 51.389]} zoom={13}>
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      </MapContainer>
+    </div>
   );
 }
 
