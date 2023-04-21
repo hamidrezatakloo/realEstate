c6aed2f06609cc07a2b1b0b6322abfd1634cc2e1
hamidreza <takloohamidreza@gmail.com>
2023-04-21 10:20:37 -0400

    use locationMarker component in LocationPicker

--- src/components/LocationPicker.js
+++ src/components/LocationPicker.js
@@ -1,10 +1,12 @@
 import { MapContainer, TileLayer } from "react-leaflet";
+import LocationMarker from "./LocationMarker";
 function LocationPicker() {
   return (
     <div>
       <p>لوکیشن خود را از روی نقشه انتخاب کنید</p>
       <MapContainer center={[35.6892, 51.389]} zoom={13}>
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <LocationMarker />
       </MapContainer>
     </div>
   );
