45cfc73b19eee3348515cef9901a13dae51c364b
hamidreza <takloohamidreza@gmail.com>
2023-04-22 09:14:53 -0400

    Update Home component to fetch and display posts

--- src/container/Home.js
+++ src/container/Home.js
@@ -1,5 +1,31 @@
+import { useState, useEffect } from "react";
 const Home = () => {
-  return <div>Home</div>;
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await fetch(
+        `http://localhost:4000/posts?_page=${1}&_limit=6`
+      );
+      const data = await response.json();
+      setPosts(data);
+    }
+    fetchPosts();
+  }, []);
+
+  return (
+    <div>
+      {posts.length === 0 ? (
+        <p>No posts found.</p>
+      ) : (
+        <ul>
+          {posts.map((post) => (
+            <li key={post.id}>{post.address}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default Home;
