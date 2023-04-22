cd0d5f8cd41e5d0a252ff94f2b041f9657b59215
hamidreza <takloohamidreza@gmail.com>
2023-04-22 09:25:05 -0400

    Add pagination to Home component
    Create state variables for currentPage and totalPages
    Update fetchPosts() to fetch data for the current page
    Add functions handlePrevPage() and handleNextPage() to update currentPage
    Conditionally render pagination buttons based on data availability

--- src/container/Home.js
+++ src/container/Home.js
@@ -1,28 +1,52 @@
 import { useState, useEffect } from "react";
 const Home = () => {
   const [posts, setPosts] = useState([]);
-
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
   useEffect(() => {
     async function fetchPosts() {
       const response = await fetch(
-        `http://localhost:4000/posts?_page=${1}&_limit=6`
+        `http://localhost:4000/posts?_page=${currentPage}&_limit=6`
       );
       const data = await response.json();
       setPosts(data);
+
+      // Extract the total number of posts from the response headers
+      const totalCount = response.headers.get("X-Total-Count");
+      setTotalPages(Math.ceil(totalCount / 6)); // Calculate the total number of pages based on the total count
     }
     fetchPosts();
-  }, []);
+  }, [currentPage]);
+
+  function handlePrevPage() {
+    setCurrentPage(currentPage - 1);
+  }
+
+  function handleNextPage() {
+    setCurrentPage(currentPage + 1);
+  }
 
   return (
     <div>
       {posts.length === 0 ? (
         <p>No posts found.</p>
       ) : (
-        <ul>
-          {posts.map((post) => (
-            <li key={post.id}>{post.address}</li>
-          ))}
-        </ul>
+        <>
+          <ul>
+            {posts.map((post) => (
+              <li key={post.id}>{post.address}</li>
+            ))}
+          </ul>
+          <button onClick={handlePrevPage} disabled={currentPage === 1}>
+            Prev
+          </button>
+          <button
+            onClick={handleNextPage}
+            disabled={currentPage === totalPages}
+          >
+            Next
+          </button>
+        </>
       )}
     </div>
   );
