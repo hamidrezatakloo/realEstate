a6c743394adb98b971547e6ddb7aa2dff4e7db2f
hamidreza <takloohamidreza@gmail.com>
2023-04-22 12:27:29 -0400

    Add Link to post page for post component

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,10 +1,17 @@
 import styles from "./post.module.css";
+import { Link } from "react-router-dom";
 const Post = ({ post }) => {
   return (
-    <div className={styles.post}>
-      <img src="./sampleImage.jpg" alt="default" className={styles.postImage} />
-      <p className={styles.address}>{post.address}</p>
-    </div>
+    <Link to={`/posts/${post.id}`} className={styles.link}>
+      <div className={styles.post}>
+        <img
+          src="./sampleImage.jpg"
+          alt="default"
+          className={styles.postImage}
+        />
+        <p className={styles.address}>{post.address}</p>
+      </div>
+    </Link>
   );
 };
 
