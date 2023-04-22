4bbb1f3661bdf29d58214762396e18afde88f898
hamidreza <takloohamidreza@gmail.com>
2023-04-22 05:31:26 -0400

    Add private route functionality for /addpost route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import styles from "./App.module.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 import UserContext from "./contexts/UserContext";
+import PrivateRoute from "./components/PrivateRoute";
 function App() {
   const [currentUser, setCurrentUser] = useState(null);
   return (
@@ -14,7 +15,14 @@ function App() {
           <Routes>
             <Route path="/login" element={<Login />}></Route>
             <Route path="/signup" element={<Signup />}></Route>
-            <Route path="/addpost" element={<AddPost />}></Route>
+            <Route
+              path="/addpost"
+              element={
+                <PrivateRoute>
+                  <AddPost />
+                </PrivateRoute>
+              }
+            ></Route>
           </Routes>
         </div>
       </UserContext.Provider>
