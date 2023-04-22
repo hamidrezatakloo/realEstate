4aad97a5ce10947f33a6b5b8cd1f72916f00bae2
hamidreza <takloohamidreza@gmail.com>
2023-04-22 04:20:45 -0400

    Provide currentUser state to UserContext.Provider

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import Signup from "./container/Signup";
 import AddPost from "./container/AddPost";
 import styles from "./App.module.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import UserContext from "./contexts/UserContext";
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
   return (
     <BrowserRouter>
-      <div className={styles.container}>
-        <Routes>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/signup" element={<Signup />}></Route>
-          <Route path="/addpost" element={<AddPost />}></Route>
-        </Routes>
-      </div>
+      <UserContext.Provider value={{ currentUser, setCurrentUser }}>
+        <div className={styles.container}>
+          <Routes>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/signup" element={<Signup />}></Route>
+            <Route path="/addpost" element={<AddPost />}></Route>
+          </Routes>
+        </div>
+      </UserContext.Provider>
     </BrowserRouter>
   );
 }
