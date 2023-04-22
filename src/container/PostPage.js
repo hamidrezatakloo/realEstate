f943a73a36345c579dca6c81d702eaa1820e59e1
hamidreza <takloohamidreza@gmail.com>
2023-04-22 15:14:21 -0400

    Fetch detail of post and display it
    This component uses the useParams hook from react-router-dom to
    get the postId parameter from the URL, and then fetches the post data
    using that ID from the json-server API.

--- src/container/PostPage.js
+++ src/container/PostPage.js
@@ -1,5 +1,36 @@
-const PostPage = () => {
-  return <div>PostPage</div>;
-};
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+function PostPage() {
+  const { postId } = useParams();
+  const [post, setPost] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/posts/${postId}`)
+      .then((response) => response.json())
+      .then((data) => setPost(data))
+      .catch((error) => console.error(error));
+  }, [postId]);
+
+  if (!post) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <img src="/sampleImage.jpg" />
+      <div>
+        <p>
+          شماره تماس:<span>{post.phoneNumber}</span>
+        </p>
+        <p>
+          آدرس:<span>{post.address}</span>
+        </p>
+        <p>
+          توضیح:<span>{post.description}</span>
+        </p>
+      </div>
+    </div>
+  );
+}
 
 export default PostPage;
